4244f99ec2a8d59bcfb8170b0c2e49638a7f011b
bouzuya <m@bouzuya.net>
2019-09-01 17:21:17 +0900

    apply UserRepository to HomeViewModel

--- app/src/main/java/net/bouzuya/sample3/HomeViewModel.kt
+++ app/src/main/java/net/bouzuya/sample3/HomeViewModel.kt
@@ -1,7 +1,39 @@
 package net.bouzuya.sample3
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import kotlinx.coroutines.launch
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel(private val _userRepository: UserRepository) : ViewModel() {
     val name = "Home"
+
+    private val _nameText = MutableLiveData<String>()
+
+    private val _userList = MutableLiveData<List<User>>()
+    val userList: LiveData<List<User>> = _userList
+
+    val userCount: LiveData<Int> = Transformations.map(_userList) { it.size }
+
+    init {
+        viewModelScope.launch {
+            _userList.value = _userRepository.findAll()
+        }
+    }
+
+    fun insert() = viewModelScope.launch {
+        val name = _nameText.value ?: return@launch
+        val user = User(0, name)
+        _userRepository.insert(user)
+
+        _userList.value = _userRepository.findAll()
+    }
+
+    fun deleteAll() = viewModelScope.launch {
+        _userRepository.deleteAll()
+
+        _userList.value = _userRepository.findAll()
+    }
+
+    fun updateNameText(s: String) {
+        _nameText.value = s
+    }
 }
