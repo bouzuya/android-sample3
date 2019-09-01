9331023568961cbc2bd4ab2bcc5aa3d90c5755bb
bouzuya <m@bouzuya.net>
2019-09-01 18:04:24 +0900

    show user list

--- app/src/main/java/net/bouzuya/sample3/BindingAdapters.kt
+++ app/src/main/java/net/bouzuya/sample3/BindingAdapters.kt
@@ -1,8 +1,13 @@
 package net.bouzuya.sample3
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.widget.EditText
 import androidx.core.widget.doAfterTextChanged
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import net.bouzuya.sample3.databinding.HomeUserListItemBinding
 
 interface EditTextAfterTextChangedListener {
     fun afterTextChanged(s: String)
@@ -13,5 +18,34 @@ fun EditText.setEditTextAfterTextChanged(listener: EditTextAfterTextChangedListe
     if (listener == null) return
     doAfterTextChanged {
         listener.afterTextChanged(it.toString())
+    }
+}
+
+@BindingAdapter("userList")
+fun RecyclerView.setUserList(userList: List<User>?) {
+    val itemList = userList ?: emptyList()
+
+    class BindingViewHolder(val binding: HomeUserListItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+    adapter = object : RecyclerView.Adapter<BindingViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingViewHolder {
+            return BindingViewHolder(
+                HomeUserListItemBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+        }
+
+        override fun getItemCount(): Int {
+            return itemList.size
+        }
+
+        override fun onBindViewHolder(holder: BindingViewHolder, position: Int) {
+            holder.binding.user = itemList[position]
+        }
     }
 }
